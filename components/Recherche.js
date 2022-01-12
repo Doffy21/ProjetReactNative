2dc5bb30c70431ace58f22d02e7e093842a4de6d
Doffy <ouattaradaoudabamory@gmail.com>
2022-01-12 01:03:42 +0100

    Fin du module de recherche

--- components/Recherche.js
+++ components/Recherche.js
@@ -1,72 +1,122 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList, ActivityIndicator, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+  Modal,
+  Button,
+  ToastAndroid,
+} from "react-native";
 import styles from "./StyleSheet";
 import { Divider, SearchBar } from "react-native-elements";
 import { useState, useEffect } from "react";
 import VilleParDefaut from "./VilleParDefaut";
-import filter from "lodash.filter"
+import filter from "lodash.filter";
 
 const Recherche = () => {
-  // const API_RESTCOUNTRIE = `https://restcountries.com/v3.1/all`
   const [search, updateSearch] = useState("");
-  const [data, setData] = useState(VilleParDefaut)
+  const [data, setData] = useState(VilleParDefaut);
   const [fullData, setFullData] = useState(data);
-  // const [isLoading, setIsLoading] = useState(false)
-  // const [error, setError] = useState(null)
+  const [modalVisibe, setModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+  const [cityFound, setCityFound] = useState({ id: "2279755", name: "Yamoussoukro", country: "CI" });
 
-  
+  useEffect(() => {
+    
+    if (modalVisibe) {
+      setIsLoading(true)
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=792989e728dbf7b2d7cf2d3596a10997`)
+        .then((response) => response.json())
+        .then((results) => {
+          setCityFound({ id: results.id, name: results.name, country: results.sys.country });
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setModalVisible(!modalVisibe)
+          setIsLoading(false);
+          alert("Vérifiez votre connexion ou la ville saisie !")
+        });      
+    }
 
-  // useEffect(() => {
-  //   setIsLoading(true)
-
-  //   fetch(API_RESTCOUNTRIE)
-  //     .then((response) => response.json())
-  //     .then((results) => {
-  //       setDatas(results);
-  //       setIsLoading(false);
-  //     })
-  //     .catch(err => {
-  //       setIsLoading(false);
-  //       setError(err);
-  //     });
-  // }, [])
-
-  // if (isLoading) {
-  //   return (
-  //     <View style={{justifyContent: 'center',alignItems: 'center',}}>
-  //       <ActivityIndicator size={"large"} color={"#5500dc"}/>
-  //     </View>
-  //   )
-  // }
-
-  // if (error) {
-  //   return (
-  //     <View style={{justifyContent: 'center',alignItems: 'center',}}>
-  //       <Text>Error fetching data... Check your network connection</Text>
-  //     </View>
-  //   )
-  // }
+  }, [modalVisibe])
 
   const separator = () => {
     return <Divider orientation="vertical" />;
   };
 
-  const handleSearch = text => {
+  const handleSearch = (text) => {
     const formattedQuery = text.toLowerCase();
-    const filteredData = filter(fullData, pays => {
-      return contains(pays, formattedQuery)
-    })
-    setData(filteredData)
+    const filteredData = filter(fullData, (pays) => {
+      return contains(pays, formattedQuery);
+    });
+    setData(filteredData);
     updateSearch(text);
-  }
+  };
 
-  const contains = ({name}, search) => {
-    name = name.toLowerCase()
+  const contains = ({ name }, search) => {
+    name = name.toLowerCase();
     if (name.includes(search)) {
-      return true
+      return true;
     }
 
-    return false
+    return false;
+  };
+
+  const submitEditing = (valueSearch) => {
+    if (valueSearch == "") {
+      ToastAndroid.show("Veuillez entrer le nom de la ville !", ToastAndroid.SHORT)
+    }
+
+    if (data.length != 0) {
+      ToastAndroid.show("Déjà dans la liste des villes !", ToastAndroid.SHORT)
+    } else {
+      setModalVisible(true);
+    }
+  };
+
+  const addNewCity = (id,name,country) => {
+    VilleParDefaut.push({id:id,name:name,country:country})
+    setModalVisible(!modalVisibe)
+    console.log(VilleParDefaut)
+  }
+
+
+  const ContainModal =() => {
+    if (isLoading) {
+      return (
+        <>
+          <ActivityIndicator size={"large"} color={"#0279CA"} />   
+        </>
+      )
+    }
+    else {
+      return (
+        <>
+          <Text style={{ marginBottom: 10 }}>
+            {cityFound.name} , {cityFound.country}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <TouchableOpacity style={{ marginRight: 10 }} onPress={() => addNewCity(cityFound.id,cityFound.name,cityFound.country)}>
+              <Text style={{ backgroundColor: "#0279CA", color: "white", padding: 10 }}>
+                AJOUTER
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setModalVisible(!modalVisibe)}>
+              <Text style={{ backgroundColor: "#ececec", color: "black", padding: 10 }}>
+                ANNULER
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      );
+    }
   }
 
   return (
@@ -75,8 +125,9 @@ const Recherche = () => {
         placeholder="Ville par défaut"
         round
         searchIcon={{ size: 24 }}
-        onChangeText={searchText => handleSearch(searchText)}
+        onChangeText={(searchText) => handleSearch(searchText)}
         value={search}
+        onSubmitEditing={() => submitEditing(search)}
         lightTheme={true}
         containerStyle={{
           marginTop: 5,
@@ -87,26 +138,37 @@ const Recherche = () => {
           padding: 0,
           borderRadius: 30,
           marginBottom: 5,
-          
         }}
         inputContainerStyle={{
-          borderRadius: 30,backgroundColor: '#ececec'
+          borderRadius: 30,
+          backgroundColor: "#ececec",
         }}
       />
+
+      <Modal animationType="fade" transparent={true} visible={modalVisibe}>
+        <View style={styles.centered}>
+          <View style={styles.modalView}>
+            <ContainModal/>
+          </View>
+        </View>
+      </Modal>
 
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={separator}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => {alert(item.name)}}>
+          <TouchableOpacity
+            onPress={() => {
+              alert(item.name);
+            }}
+          >
             <View style={{ padding: 10, borderBottomColor: "white" }}>
               <Text style={{ fontSize: 18 }}>
                 {item.name} , {item.country}
               </Text>
-            </View>            
+            </View>
           </TouchableOpacity>
-
         )}
       />
     </View>
--- components/StyleSheet.js
+++ components/StyleSheet.js
@@ -1,13 +1,30 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },   
-    centered: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    } 
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  centered: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    width:200,
+    height:150,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
