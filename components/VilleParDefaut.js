e7c86bc29a71d9d690323da639b3f9e6d5e52b32
Doffy <ouattaradaoudabamory@gmail.com>
2022-01-15 12:14:56 +0100

    Mise à jour de l'application

--- components/VilleParDefaut.js
+++
@@ -1,9 +0,0 @@
-const villeParDefaut= [
-    { id: 2293538, name: "Abidjan", country: "CI" },
-    { id: 2643743, name: "London", country: "GB" },
-    { id: 3117735, name: "Madrid", country: "ES" },
-    { id: 2988507, name: "Paris", country: "FR" },
-    { id: 2279755, name: "Yamoussoukro", country: "CI" },
-]
-
-export default villeParDefaut;
